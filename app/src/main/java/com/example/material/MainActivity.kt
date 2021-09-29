3c2ca5a08bab9ef89bc520d24a7142812682a606
leif2k <leif2k@yandex.ru>
2021-09-29 20:47:36 +0300

    TopAppBar + Bottom Navigation

--- app/src/main/java/com/example/material/MainActivity.kt
+++ app/src/main/java/com/example/material/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.material
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
 import com.example.material.databinding.ActivityMainBinding
 
@@ -14,12 +16,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        setSupportActionBar(binding?.topAppBar)
+
         supportFragmentManager.beginTransaction().replace(R.id.content, Home()).commit()
         binding?.bottomNav?.selectedItemId = R.id.homeItemBottomNav
 
         binding?.bottomNav?.setOnItemSelectedListener { item ->
-
-            when(item.itemId){
+            when (item.itemId) {
                 R.id.homeItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Home()).commit()
                 R.id.shopItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Shop()).commit()
                 R.id.deliveryItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Delivery()).commit()
@@ -30,6 +33,37 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+
+        binding?.topAppBar?.setOnMenuItemClickListener { menuItem: MenuItem ->
+
+            when (menuItem.itemId) {
+                R.id.favoritesTopItemId -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.content, Favorites()).commit()
+                    true
+                }
+
+                R.id.settingsTopItemId -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.content, Settings()).commit()
+                    true
+                }
+
+                R.id.cloudTopItemId -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.content, Cloud()).commit()
+                    true
+                }
+
+                else -> false
+            }
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.top_menu, menu)
+
+        return true
 
     }
 }
